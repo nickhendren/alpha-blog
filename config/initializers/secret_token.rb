bb7b3f7d3d9a7127c75217c40b90ef5c99f54d53
Nick Hendren <nick@ravecrate.com>
2017-02-20 14:31:15 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlphaBlog::Application.config.secret_token = '5f7997e0a432015bd85f3e6c529c08382e13e5f07cda153f0182e466f44e3d024a9a92514dee5c4d3187f2f42ead8c9b01a45887c2e4c5396f3c6d2366c06338'
