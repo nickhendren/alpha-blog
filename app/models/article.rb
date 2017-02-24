3914bef2bd656db17b489041b8d4e90c6778857e
Nick Hendren <nhendren7@gmail.com>
2017-02-24 14:40:44 -0800

    Add user and article association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
+
   attr_accessible :title, :description
 
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true, length: {minimum: 10, maximum: 300}
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20170224202709) do
+ActiveRecord::Schema.define(:version => 20170224220131) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
