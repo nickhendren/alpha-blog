754265642cf8a0f059a2d228c96a09230e277da9
Nick Hendren <nhendren7@gmail.com>
2017-02-24 13:11:02 -0800

    Create user table and model

---
+++ db/migrate/20170224202709_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def up
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.timestamps
+    end
+  end
+
+  def down
+  end
+end
